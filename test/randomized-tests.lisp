f01e0d04c6d9dadf72300ce71ffa3780d932dcd9
DalekBaldwin <DalekBaldwin@users.noreply.github.com>
2015-02-24 04:19:41 -0800

    test different size ranges for ints

--- test/randomized-tests.lisp
+++ test/randomized-tests.lisp
@@ -13,26 +13,28 @@
                  #+abcl t ;; ridiculous
                  )))))
 
-;;;; Shrink results of generators
-
 (deftest test-int-generate ()
-  (let ((*size* 10))
-    (let ((generator (generator (integer * 2))))
-      (loop for i from 1 to 100
-           do
-           (is (<= -10 (generate generator) 2))))
-    (let ((generator (generator (integer -2 *))))
-      (loop for i from 1 to 100
-           do
-           (is (<= -2 (generate generator) 10))))
-    (let ((generator (generator (integer -2 2))))
-      (loop for i from 1 to 100
-           do
-           (is (<= -2 (generate generator) 2))))
-    (let ((generator (generator (integer))))
-      (loop for i from 1 to 100
-           do
-           (is (<= -10 (generate generator) 10))))))
+  (loop for size in (list 10 20 50 300)
+     do
+       (let ((*size* size))
+         (let ((generator (generator (integer * 2))))
+           (loop for i from 1 to 100
+              do
+                (is (<= (- *size*) (generate generator) 2))))
+         (let ((generator (generator (integer -2 *))))
+           (loop for i from 1 to 100
+              do
+                (is (<= -2 (generate generator) *size*))))
+         (let ((generator (generator (integer -2 2))))
+           (loop for i from 1 to 100
+              do
+                (is (<= -2 (generate generator) 2))))
+         (let ((generator (generator (integer))))
+           (loop for i from 1 to 100
+              do
+                (is (<= (- *size*) (generate generator) *size*)))))))
+
+;;;; Shrink results of generators
 
 (deftest test-int-generate-shrink ()
   (let ((generator (generator (guard #'positive-integer-p (integer)))))
