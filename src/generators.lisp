17ef885c5121c2357c9538b881165f2c610ae79d
Sebastian Christ <rudolfo.christ@gmail.com>
2015-04-29 13:27:48 +0200

    add generator to create alphabetical characters

--- src/generators.lisp
+++ src/generators.lisp
@@ -448,3 +448,7 @@
                     (macrolet ((,gen-form ()
                                  `(progn ,@(list ,@body))))
                       (,gen-form)))))))))
+
+(defgenerator alpha ()
+  `(generator (or (character 65 91)
+                  (character 97 123))))
